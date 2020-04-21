825fdca79d47e7550fff5ebf8e3ca4214d5262ac
Gleb Gribkov <ggv@begarat.com>
2020-04-21 10:35:36 +0300

    Добавьте файлы проекта.

---
+++ AsteroidGame/BaseObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace AsteroidGame
+{
+    class BaseObject
+    {
+        protected Point pos;
+        protected Point dir;
+        protected Size size;
+
+        public BaseObject(Point pos, Point dir, Size size)
+        {
+            this.pos = pos;
+            this.dir = dir;
+            this.size = size;
+        }
+
+        virtual public void Draw()
+        {
+            Game.buffer.Graphics.DrawEllipse(Pens.White, pos.X, pos.Y, size.Width, size.Height);
+        }
+
+        virtual public void Update()
+        {
+            pos.X = pos.X + dir.X;
+            pos.Y = pos.Y + dir.Y;
+            if (pos.X < 0) dir.X = -dir.X;
+            if (pos.X > Game.Width) dir.X = -dir.X;
+            if (pos.Y < 0) dir.Y = -dir.Y;
+            if (pos.Y > Game.Height) dir.Y = -dir.Y;
+        }
+    }
+}
---
+++ AsteroidGame/Game.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AsteroidGame
+{
+    
+
+    static class Game
+    {
+        static BaseObject[] objs;
+
+        static BufferedGraphicsContext context;
+        static public BufferedGraphics buffer;
+        
+        // Свойства
+        // Ширина и высота игрового поля
+        static public int Width { get; set; }
+        static public int Height { get; set; }
+
+        static Game()
+        {
+        }
+
+        static public void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // предоставляет доступ к главному буферу графического контекста для текущего приложения
+            context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();// Создаём объект - поверхность рисования и связываем его с формой
+                                      // Запоминаем размеры формы
+            Width = form.Width;
+            Height = form.Height;
+            // Связываем буфер в памяти с графическим объектом.
+            // для того, чтобы рисовать в буфере
+            buffer = context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Load();
+
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        static public void Load()
+        {
+
+            objs = new BaseObject[30];
+            for (int i = 0; i < 10; i++)
+                objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+            for (int i = 10; i < 20; i++)
+                objs[i] = new Star(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+            for (int i = 20; i < 30; i++)
+                objs[i] = new Rock(new Point(600, i * 20), new Point(25 - i, 26 - i), new Size(20, 20));
+
+
+
+                //            objs = new BaseObject[30];
+                //            for (int i = 0; i < objs.Length; i++)
+                //                objs[i] = new Star(new Point(600, i * 20), new Point(-i, 1), new Size(20, 20));
+
+                //            objs = new BaseObject[30];
+                //            for (int i = 0; i < objs.Length; i++)
+                //                objs[i] = new BaseObject(new Point(600, i * 20), new Point(15 - i, 15 - i), new Size(20, 20));
+        }
+
+        static public void Draw()
+        {
+            buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in objs)
+                obj.Draw();
+            buffer.Render();
+        }
+
+        static public void Update()
+        {
+            foreach (BaseObject obj in objs)
+                obj.Update();
+        }
+
+    }
+}
+
